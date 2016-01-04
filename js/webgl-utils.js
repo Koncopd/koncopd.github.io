aa0edd92c8ed7ce610ac09b770ea1cd567d0d99f
Koncopd <koncopd@gmail.com>
2016-01-05 02:54:38 +0300

    cmt

--- js/webgl-utils.js
+++ js/webgl-utils.js
@@ -52,13 +52,18 @@ function setAttributes3(gl, attrArray, bufferArray)
 function initTexture(gl)
 {
   var texture = gl.createTexture();
-  texture.image = document.getElementById('texture');
-  gl.bindTexture(gl.TEXTURE_2D, texture);
-  gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
-  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, texture.image);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-  gl.bindTexture(gl.TEXTURE_2D, null);
+  texture.image = new Image();
 
+  texture.image.onload = function()
+  {
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, texture.image);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+    gl.bindTexture(gl.TEXTURE_2D, null);
+  }
+  
+  texture.image.src = './pyramid.gif';
   return texture;
 }
