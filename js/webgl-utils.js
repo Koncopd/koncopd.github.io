7954e562935c532a79d98089d674899aa88b88a1
Koncopd <koncopd@gmail.com>
2016-01-05 04:16:42 +0300

    jpg texture

--- js/webgl-utils.js
+++ js/webgl-utils.js
@@ -62,6 +62,6 @@ function initTexture(gl)
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
     gl.bindTexture(gl.TEXTURE_2D, null);
   }
-  texture.image.src = './glass.gif';
+  texture.image.src = './texture.jpg';
   return texture;
 }
